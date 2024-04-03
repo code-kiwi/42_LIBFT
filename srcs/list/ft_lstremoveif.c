deb9d0741af874e4e40a685f0ad95691a588777c
mhotting <madric.hottinger.101@gmail.com>
2024-04-03 11:56:06 +0200

    Corrects infinite loop inside of ft_lst_remove_if

--- srcs/list/ft_lstremoveif.c
+++ srcs/list/ft_lstremoveif.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 11:13:16 by mhotting          #+#    #+#             */
-/*   Updated: 2024/04/03 11:45:50 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/04/03 11:55:21 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ void	ft_lst_remove_if(
 			prev->next = node->next;
 			ft_lstdelone(node, del);
 			node = prev->next;
+			continue ;
 		}
+		node = node->next;
 	}
 }
