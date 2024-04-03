5bb3a6c97d083433bc53da73e0c1263276007179
mhotting <madric.hottinger.101@gmail.com>
2024-04-03 10:18:27 +0200

    Adds ft_lstfind.c file

---
+++ srcs/list/ft_lstfind.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_listfind.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 10:11:31 by mhotting          #+#    #+#             */
+/*   Updated: 2024/04/03 10:16:20 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_listfind(t_list *lst, void *data_ref, int *(*cmp)(void *))
+{
+	if (lst == NULL || cmp == NULL)
+		return (NULL);
+	while (lst != NULL)
+	{
+		if (cmp(lst->content, data_ref) == 0)
+			return (lst);
+		lst = lst->next;
+	}
+	return (NULL);
+}
